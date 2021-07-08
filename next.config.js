558a2e18515e9a53e1324ed44c9e16419e92e6fe
Joey Kanaly <me@joeykanaly.com>
2021-07-08 11:34:43 -0500

    First 7 bosses

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-  reactStrictMode: true,
-}
+// next.config.js
+const withMDX = require('@next/mdx')({
+	extension: /\.mdx$/,
+});
+module.exports = withMDX({
+	pageExtensions: ['js', 'jsx', 'mdx'],
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../styles/markdown.css';
+import { MDXProvider } from '@mdx-js/react';
+import Link from 'next/link';
+import Script from 'next/script';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const CustomLink = (props) => {
+	const href = props.href;
+	const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
+
+	if (isInternalLink) {
+		return (
+			<Link href={href}>
+				<a {...props}>{props.children}</a>
+			</Link>
+		);
+	}
+
+	return <a target='_blank' rel='noopener noreferrer' {...props} />;
+};
+
+const components = {
+	a: CustomLink,
+};
+
+export default function App(app) {
+	const { Component, pageProps } = app;
+	return (
+		<MDXProvider components={components}>
+			<Component {...pageProps} />
+			<Script src='https://wow.zamimg.com/widgets/power.js'></Script>
+		</MDXProvider>
+	);
 }
-
-export default MyApp
